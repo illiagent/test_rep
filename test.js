c447106a33f6f610e4b4c6bc6acaa388f2861eae
ILLIA <illia@MacBook-Pro-ILLIA.local>
2020-03-16 20:49:12 +0300

    s

--- test.js
+++ test.js
@@ -2,11 +2,9 @@ function sum(a, b) {
     return a + b
 }
 (function () {
-<<<<<<< Updated upstream
     console.log(`summa 2+3= ${sum(2, 3)}`})()
-=======
-    alert(`summa 2+3= ${sum(2, 3)}`)
-})()
+alert(`summa 2+3= ${sum(2, 3)}`)
+}) ()
 
 function print(message) {
     console.log('message')
@@ -15,4 +13,3 @@ function print(message) {
 let sum = (function () {
     return (a, b) => a + b;
 })()
->>>>>>> Stashed changes
